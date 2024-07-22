e2355251ea7f146e2747e181c83c09b1bc07b5cb
Isak Fagerlund <fagerlund.isak@gmail.com>
2024-07-23 00:10:52 +0200

    tried clerk, does not work offline

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ import { getEntriesLocal } from './lib/queries.ts';
 function App({ queryClient }: { queryClient: QueryClient }) {
   return (
     <div className="bg-blue-800 h-dvh">
+      {/* Signed out
+        <div className="container justify-center flex flex-col gap-8 items-center h-dvh">
+          <h1 className="text-3xl font-bold text-blue-100">Daily Reflection</h1>
+            <Button className="w-[250px]">Sign in</Button>
+        </div>
+      */}
       <QueryClientProvider client={queryClient}>
         <Home />
       </QueryClientProvider>
